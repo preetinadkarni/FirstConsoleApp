25ce9b508b2c483216630515f0097c1dbaf7f573
preetinadkarni <61760445+preetinadkarni@users.noreply.github.com>
2021-11-18 10:19:00 +1300

    Step 10 (#5)
    * step-8 unit tests
    
    * step-8 fix unit tests
    
    * step-9 unit tests
    
    * step-9 green code
    
    * use lambda instead of delegate
    
    * step 10 - red code
    
    * Green Code
    
    * Green Code
    
    * refactor
    
    * rework review comments
    
    Co-authored-by: Preeti.Nadkarni <preeti.nadkarni@serko.com>

--- FirstConsoleApp/StringCalculator.cs
+++ FirstConsoleApp/StringCalculator.cs
@@ -1,5 +1,6 @@
 using System.Net.Sockets;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FirstConsoleApp
@@ -13,39 +14,49 @@ namespace FirstConsoleApp
             if (!input.Any(Char.IsDigit)) return 0;
 
             int[] numbers = GetNumbers(input);
-            int sum = 0;
-
-            int[] negativeNumbers = GetNegativeNumbers(numbers);
+            CheckInputHasNegativeNumbers(numbers);
             
-            if(negativeNumbers.Length > 0 )
-            {
-                throw new ArgumentException("Negatives not allowed: " + string.Join(", ", negativeNumbers));
-            }
             return numbers.Sum();
         }
 
-        private static int[] GetNegativeNumbers(int[] numbers)
+        private void CheckInputHasNegativeNumbers(int[] numbers)
+        {
+            int[] negativeNumbers = GetNegativeNumbers(numbers);
+
+            if (negativeNumbers.Length > 0)
+            {
+                throw new ArgumentException("Negatives not allowed: " + string.Join(", ", negativeNumbers));
+            }
+        }
+        private int[] GetNegativeNumbers(int[] numbers)
         {
             return Array.FindAll(numbers, n => n < 0);
         }
 
+        private int[] GetNumbersLessThanThousand(int[] numbers)
+        {
+            return Array.FindAll(numbers, n => n < 1000);
+        }
+
         private int[] GetNumbers(string input)
         {
-            string[] numbers;
-            bool isSeparatorSpecifiedInInput = input.StartsWith("//");
-            if (isSeparatorSpecifiedInInput)
+            string[] sNumbers;
+            bool isDelimiterSpecifiedInInput = input.StartsWith("//");
+            if (isDelimiterSpecifiedInInput)
             {
-                string separator = GetSeparator(input);
+                string[] delimiters = GetDelimiters(input);
                 string numbersList = GetNumbersFromInput(input);
-                numbers = numbersList.Split(separator);
+                sNumbers = numbersList.Split(delimiters, StringSplitOptions.None);
             }
             else
             {
-                char[] defaultSeparators = {',', '\n'};
-                numbers = input.Split(defaultSeparators);
+                string[] defaultDelimiters = {",", "\n"};
+                sNumbers = input.Split(defaultDelimiters,  StringSplitOptions.None);
             }
 
-            return Array.ConvertAll(numbers,int.Parse);
+            int[] iNumbers = Array.ConvertAll(sNumbers, int.Parse);
+            iNumbers = GetNumbersLessThanThousand(iNumbers);
+            return iNumbers;
         }
 
         private string GetNumbersFromInput(string input)
@@ -55,12 +66,31 @@ namespace FirstConsoleApp
             return numbersList;
         }
 
-        private string GetSeparator(string input) 
+        private string[] GetDelimiters(string input)
         {
-            return input.Substring(2,1);
+            List<string> delimiters = new List<string>();
+            bool hasMultipleDelimiters = input.IndexOf('[') > 0;
+            if (hasMultipleDelimiters)
+            {
+                delimiters = GetMultipleDelimitersInAnArray(input);
+
+            }
+            else
+                delimiters.Add(input.Substring(2, 1));
+
+            return delimiters.ToArray();
+        }
+
+        private List<string> GetMultipleDelimitersInAnArray(string input)
+        {
+            int startIndex = input.IndexOf('[');
+            int endIndex = input.LastIndexOf(']') + 1 ;
+            string allDelimitersInInput = input.Substring(startIndex,endIndex - startIndex );
+            
+            List<string> delimiters = new List<string>(allDelimitersInInput.Split('[',']'));
+            delimiters.Remove("");
+            return delimiters;
         }
         
     }
-    
-    
 }
--- UnitTests/StringCalculatorUnitTests.cs
+++ UnitTests/StringCalculatorUnitTests.cs
@@ -44,11 +44,38 @@ namespace UnitTests
             return _stringCalculator.Add(input);
         }
         
+        [TestCase("//;\n1;2", ExpectedResult=3)]
+        [TestCase("//:\n3:5:3:9", ExpectedResult=20)]
+        public int GivenInputHasDelimiterWhenAddThenReturnsSumOfNumbers(string input)
+        {
+            return _stringCalculator.Add(input);
+        }
+        
         [Test]
         public void GivenInputHasNegativeNumberWhenAddThenReturnsAnException()
         {
-            Exception ex = Assert.Throws<System.ArgumentException>(delegate() { _stringCalculator.Add("-1,2,-3");});
+            Exception ex = Assert.Throws<System.ArgumentException>(() => { _stringCalculator.Add("-1,2,-3");});
             Assert.AreEqual(ex.Message,"Negatives not allowed: -1, -3");
+        }
+        
+        [TestCase("1000,1001,2", ExpectedResult=2)]
+        public int GivenInputHasNumbersGreaterThanThousandWhenAddThenIgnoreThemInSum(string input)
+        {
+            return _stringCalculator.Add(input);
+        }
+        
+        [TestCase("//[***]\n1***2***3", ExpectedResult=6)]
+        public int GivenInputHasDelimiterOfAnyLengthWhenAddThenReturnSum(string input)
+        {
+            return _stringCalculator.Add(input);
+        }
+        
+        [TestCase("//[*][%]\n1*2%3", ExpectedResult=6)]
+        [TestCase("//[***][#][%]\n1***2#3%4", ExpectedResult=10)]
+        [TestCase("//[*1*][%]\n1*1*2%3", ExpectedResult=6)]
+        public int GivenInputHasMultipleDelimitersWhenAddThenReturnSum(string input)
+        {
+            return _stringCalculator.Add(input);
         }
     }
 }
